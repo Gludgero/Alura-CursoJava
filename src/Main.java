c789385561aa3d71ca8daf294cc71b4c41d993f8
GLudgero <guiludgero@sj3.com.br>
2024-06-03 20:55:19 -0300

    Calculadora de Celsius para Farenheint

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        double tempC = 36.7;
+        double tempF = 0.0;
+
+        tempF = (tempC * 1.8) + 32;
+
+        int tempFF = (int) tempF;
+
+        System.out.println(tempFF);
+
+
+
+
+
+
+
+        }
+    }
