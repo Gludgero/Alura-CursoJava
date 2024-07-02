54af42272f7cf66bcab931c8f56d5cf628291fc5
GLudgero <guiludgero@sj3.com.br>
2024-07-02 19:07:50 -0300

    projeto final, sistema de gerenciamento de conta corrente

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double tempC = 36.7;
-        double tempF = 0.0;
+        Scanner scanner = new Scanner(System.in);
 
-        tempF = (tempC * 1.8) + 32;
+        String nome = "Guilherme Ludgero";
+        String tipoConta = "Corrente";
+        double saldo = 2500.00;
+        int n = 0;
 
-        int tempFF = (int) tempF;
+        // Mensagem inicial - \n
+        System.out.println("*******************************\nDados iniciais do cliente:\n\n");
+        System.out.println("Nome: " + nome);
+        System.out.println("\nTipo de conta: " + tipoConta);
+        System.out.println("\nSaldo inicial: " + saldo);
+        System.out.println("*******************************\n");
 
-        System.out.println(tempFF);
+        // Menu de opções -
+
+        while(n != 4) {
+
+            System.out.println("Por favor, escolha uma das seguintes operações:");
+            System.out.println("\n1- Consultar saldos\n2- Receber Valores\n3- Trasferir valor\n4- Sair");
+
+            n = scanner.nextInt();
+
+        if (n == 1) {
+            System.out.println("O saldo atual é de: " + saldo);
+
+        } else if (n == 2) {
+            System.out.println("Informe o valor a receber: ");
+            saldo = saldo + scanner.nextDouble();
+            System.out.println("O novo saldo é de: " + saldo);
+
+        } else if ( n == 3) {
+            System.out.println("Informe o valor a ser transferido: ");
+            double transf = scanner.nextDouble();
+
+            if (saldo >= transf) {
+                saldo = saldo - transf;
+                System.out.println("O novo saldo é de: " + saldo);
+            } else {
+                System.out.println("Por favor, digite um valor válido!");
+            }
+        } else if (n != 4){
+            System.out.println("Número inválido! Digite um caractere válido");
+        }
+
+        }
 
 
 
@@ -15,5 +57,14 @@ public class Main {
 
 
 
+
+
+
+
+
+
+
+
+//______________________________________________________________________________________________
         }
     }
